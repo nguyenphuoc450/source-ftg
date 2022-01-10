c6cb894c905836e3e93c40f3b4ade5b8f7205ab1
phuocnv <iloveyoumayba@gmail.com>
2022-01-10 16:54:20 +0700

    page login, register

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,26 @@
 <template>
-  <div>
-      Login
-  </div>
+  <section>
+      <div class="wrapper">
+        <div class="image"></div>
+        <div class="wrapper-form">
+          <img src="../assets/logo-footer.png" alt="">
+          <h1 class="title">
+            Log in to FreeToGame
+          </h1>
+          <form action="">
+            <div class="form-group">
+              <input type="text" name="username" placeholder="Username">
+            </div>
+            <div class="form-group">
+              <input type="password" name="password" placeholder="Password">
+            </div>
+            <button class="form-btn">Login</button>
+            <div class="border-line"></div>
+            <p class="form-link">Not a member yet? <router-link v-bind:to="{ name: 'Register' }"> Create Account <i class="fas fa-chevron-right small"></i></router-link></p>
+          </form>
+        </div>
+      </div>
+  </section>
 </template>
 
 <script>
@@ -10,6 +29,81 @@ name: 'Login'
 }
 </script>
 
-<style>
 
+<style lang="scss" scoped>
+@import '../assets/styles/color.scss';
+.wrapper {
+  max-width: 990px;
+  margin: 48px auto;
+  display: grid;
+  grid-template-columns: 50% 50%;
+  background-color: #32383e;
+  .image {
+    background-image: url(../assets/login.jpg);
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+  }
+  .wrapper-form {
+    padding: 48px;
+    img {
+      display: block;
+      margin: 0 auto 16px;
+    }
+    .title {
+      text-align: center;
+      font-size: 20px;
+      margin-bottom: 24px;
+    }
+    .form-group {
+      margin-bottom: 16px;
+      input {
+        display: block;
+        width: 100%;
+        padding: 12px 16px;
+        background-color: #1c1e22;
+        border: 1px solid rgba(0,0,0,.6);
+        border-radius: 4px;
+        outline: none;
+        font-size: 14px;
+        font-weight: 400;
+        line-height: 1.5;
+        color: $color-gray;
+        transition: all ease 0.3s;
+        &:focus {
+          border-color: $color-blue;
+        }
+      }
+    }
+    .form-btn {
+      display: block;
+      width: 100%;
+      padding: 12px 16px;
+      color: #fff;
+      background-color: #3a3f44;
+      border: 1px solid #22241d;
+      text-align: center;
+      font-size: 18px;
+      line-height: 1.5;
+      border-radius: 4px;
+      transition: all ease 0.3s;
+      cursor: pointer;
+      &:hover {
+        background-color: #22241d;
+      }
+    }
+  }
+  .border-line {
+    margin: 16px 0;
+    border-top: 1px solid rgba(94,84,84,.1);
+  }
+  .form-link {
+    text-align: center;
+    font-size: 12px;
+    color: $text-color;
+    a {
+      color: $color-blue;
+    }
+  }
+}
 </style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,7 +1,30 @@
 <template>
-  <div>
-      Register
-  </div>
+  <section>
+      <div class="wrapper">
+        <div class="image"></div>
+        <div class="wrapper-form">
+          <h1 class="title">
+            Create My Account!
+          </h1>
+          <form action="">
+            <div class="form-group">
+              <input type="text" name="username" placeholder="Username">
+            </div>
+            <div class="form-group">
+              <input type="email" name="email" placeholder="Email Address">
+            </div>
+            <div class="form-group row">
+              <input type="password" name="password" placeholder="Password">
+              <input type="password" name="confirm-password" placeholder="Confirm Password">
+            </div>
+            <button class="form-btn">Create Account</button>
+            <p class="form-note">This site is protected by reCAPTCHA and the Google Privacy Policy and Terms of Service apply.</p>
+            <div class="border-line"></div>
+            <p class="form-link">Already a member? <router-link v-bind:to="{ name: 'Login' }"> Log In <i class="fas fa-chevron-right small"></i></router-link></p>
+          </form>
+        </div>
+      </div>
+  </section>
 </template>
 
 <script>
@@ -10,6 +33,87 @@ name:'Register'
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+@import '../assets/styles/color.scss';
+.wrapper {
+  max-width: 1000px;
+  margin: 48px auto;
+  display: grid;
+  grid-template-columns: 45% 55%;
+  background-color: #32383e;
+  .image {
+    background-image: url(../assets/register.jpg);
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+  }
+  .wrapper-form {
+    padding: 48px;
+    .title {
+      text-align: center;
+      font-size: 20px;
+      margin-bottom: 24px;
+    }
+    .form-group {
+      margin-bottom: 16px;
+      &.row {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        grid-gap: 12px;
+      }
+      input {
+        display: block;
+        width: 100%;
+        padding: 12px 16px;
+        background-color: #1c1e22;
+        border: 1px solid rgba(0,0,0,.6);
+        border-radius: 4px;
+        outline: none;
+        font-size: 14px;
+        font-weight: 400;
+        line-height: 1.5;
+        color: $color-gray;
+        transition: all ease 0.3s;
+        &:focus {
+          border-color: $color-blue;
+        }
+      }
+    }
+    .form-btn {
+      display: block;
+      width: 100%;
+      padding: 12px 16px;
+      color: #fff;
+      background-color: #3a3f44;
+      border: 1px solid #22241d;
+      text-align: center;
+      font-size: 18px;
+      line-height: 1.5;
+      border-radius: 4px;
+      transition: all ease 0.3s;
+      cursor: pointer;
+      &:hover {
+        background-color: #22241d;
+      }
+    }
+    .form-note {
+      margin-top: 8px;
+      font-size: 12px;
+      color: $color-gray;
+      font-weight: 400;
+    }
+  }
+  .border-line {
+    margin: 16px 0;
+    border-top: 1px solid rgba(94,84,84,.1);
+  }
+  .form-link {
+    text-align: center;
+    font-size: 12px;
+    color: $text-color;
+    a {
+      color: $color-blue;
+    }
+  }
+}
 </style>
